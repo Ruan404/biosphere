28d365d3f61d357af9998f2e1dde84a374fa7a83
ruan404 ruan404 <notezero231@gmail.com>
2025-03-03 21:01:24 +0100

    add signup and logout system

--- src/Auth/AuthController.php
+++ src/Auth/AuthController.php
@@ -17,7 +17,8 @@ class AuthController
     private Layout $layout = Layout::Auth;
     private $authService;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->authService = new AuthService();
     }
 
@@ -40,7 +41,7 @@ class AuthController
             $user = $this->authService->login($loginUser);
 
             if ($user) {
-                header('Location: /' ); //le homeController prend le relais
+                header('Location: /'); //le homeController prend le relais
                 exit();
             }
         }
@@ -51,11 +52,37 @@ class AuthController
     #[Route(method: "GET", path: "/signup")]
     public function signupPage()
     {
-
+        Page::print(view: 'auth/signup', layout: $this->layout);
     }
 
     #[Route(method: "POST", path: "/signup")]
     public function signup()
     {
+        if (!empty($_POST)) {
+
+            //create a new user
+            $signupUser = new User();
+            $signupUser->pseudo = $_POST['pseudo'];
+            $signupUser->mdp = $_POST['password'];
+
+            //essayer d'inscrire l'utilisateur
+            $result = $this->authService->signup($signupUser);
+
+            if ($result == true) {
+                header('Location: /login');
+                exit();
+            }
+
+            Page::print(view: 'auth/signup', layout: $this->layout, infos: ['error' => true]);
+        }
+    }
+
+    #[Route(method: "GET", path: "/logout")]
+    public function logout()
+    {
+        $this->authService::logout();
+
+        header('Location: /login');
+        exit();
     }
 }
