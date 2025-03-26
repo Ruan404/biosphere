6b405339b0698e7e146ea5523c65119989d1c038
ruan404 <notezero231@gmail.com>
2025-03-26 13:52:37 +0100

    add admin

--- src/Film/FilmService.php
+++ src/Film/FilmService.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Film;
+
 use App\Core\Database;
 use PDO;
 
 class FilmService
 {
 
-    public function getAllFilms():?array
+    public function getAllFilms(): ?array
     {
         $query = Database::getPDO()->query('SELECT cover, title FROM film JOIN genre ON film.genre_id = genre.id');
 
@@ -16,17 +17,25 @@ class FilmService
         return $films;
     }
 
-    public function getFilmByTitle($title): ?array{
-        $query = Database::getPDO()->prepare('SELECT film.title, film.cover, film.video, film.description, genre.name FROM film JOIN genre ON film.genre_id = genre.id WHERE film.title = :title');
+    public function getFilmByTitle($title): ?array
+    {
+        $query = Database::getPDO()->prepare('SELECT * FROM film JOIN genre ON film.genre_id = genre.id WHERE film.title = :title');
         $query->bindParam(':title', $title, PDO::PARAM_STR);
         $query->execute();
         $film = $query->fetch(PDO::FETCH_ASSOC);
 
-        if($film){
+        if ($film) {
             return $film;
         }
 
         return null;
-        
+    }
+
+    public function deleteFilm($filmId)
+    {
+        $query = Database::getPDO()->prepare('DELETE FROM film WHERE id = ?');
+        $result = $query->execute([$filmId]);
+
+        return $result;
     }
 }
